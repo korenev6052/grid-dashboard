01135690374ead9ac14c5bfcc4aea242d5fc0d48
Aleksandr Korenev <korenev6052@gmail.com>
2020-05-14 19:34:40 +0300

    init components structure

---
+++ src/app/grid/grid.module.ts
@@ -0,0 +1,22 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {GridsterModule} from 'angular-gridster2';
+// app imports
+import {GridComponent} from './components/grid/grid.component';
+import {WidgetComponent} from './components/widget/widget.component';
+import {SectionComponent} from './components/section/section.component';
+import {GridItemComponent} from './components/grid-item/grid-item.component';
+
+@NgModule({
+  declarations: [
+    GridComponent,
+    WidgetComponent,
+    SectionComponent,
+    GridItemComponent,
+    GridsterModule,
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class GridModule {}
